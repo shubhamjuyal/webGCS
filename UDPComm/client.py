b00a93e6ffbdd0fe3f15cda2e48ae9fee7554847
Anshu Saini <65893784+anshu189@users.noreply.github.com>
2023-04-02 19:55:33 +0530

    Updated Client.py

--- UDPComm/client.py
+++ UDPComm/client.py
@@ -1,21 +1,31 @@
+# from pymavlink import mavutil
 from pymavlink import mavutil
 import json
 import socket
-import time
+from time import sleep as s
+
+# UDP IP/PORT CONFIGURATIONS
 UDP_IP = "127.0.0.1"
 UDP_PORT = 14550
 
-TCP_IP = "127.0.0.1"
-TCP_PORT = 8000
-
 # Connect to the MAVLink UDP stream
-mavlink_conn = mavutil.mavlink_connection('udp:{0}:{1}'.format(UDP_IP, UDP_PORT))
+mavlink_conn = mavutil.mavlink_connection(f'udp:{UDP_IP}:{UDP_PORT}')
 
-# Connect to the TCP server
-tcp_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-tcp_sock.connect((TCP_IP, TCP_PORT))
 
-# # Send a MAVLink message to the server
+# # TCP IP/PORT SERVER CONFIGURATIONS
+# TCP_IP = "127.0.0.1"
+# TCP_PORT = 8000
+
+# # Connect to the TCP server
+# tcp_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# tcp_sock.connect((TCP_IP, TCP_PORT))
+
+UDP_IP = "127.0.0.1" # Change to your desired IP address
+UDP_PORT = 5005 # Change to your desired port number
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+
+# # Send a MAVLink message to the UDP server back
 # msg = mavutil.mavlink.MAVLink_heartbeat_message(
 #     mavutil.mavlink.MAV_TYPE_GCS,
 #     mavutil.mavlink.MAV_AUTOPILOT_INVALID,
@@ -24,15 +34,25 @@ tcp_sock.connect((TCP_IP, TCP_PORT))
 #     0)
 # mavlink_conn.send(msg)
 
-# Receive a MAVLink message from the UDP stream
+
+# Receive a MAVLink message from the UDP stream and send it to TCP server
 while True:
-    msg = mavlink_conn.recv_msg()
-    if msg is not None:
-        # Convert the MAVLink message to a dictionary
+    msg = mavlink_conn.recv_match()
+    if msg:
         msg_dict = msg.to_dict()
-        # Convert the dictionary to a JSON-formatted string
+        
+        # To get Raw DATA
+        # print(msg_dict) 
+
         json_msg = json.dumps(msg_dict)
-        encoded_json_msg = json_msg.encode()
-        # Send the JSON-formatted message to the TCP server
-        tcp_sock.sendall(encoded_json_msg)
-        time.sleep(0.0000000001)
+        fmsg=json.loads(json_msg)    
+    
+        if fmsg.get("mavpackettype") == 'GLOBAL_POSITION_INT':
+            # Here we are only fetching lat & lon specifically (can be changed further for our requirements) 
+            coord={'lat': fmsg.get('lat')/1e7, 'lon': fmsg.get('lon')/1e7}
+            formattedcoord=json.dumps(coord)
+            maindata=formattedcoord.encode('utf-8')
+            sock.sendto(maindata, (UDP_IP, UDP_PORT))
+            print(f"Lat: {fmsg.get('lat')/1e7} Lon: {fmsg.get('lon')/1e7}")
+            print(f"Time Boot: {fmsg.get('time_boot_ms')}")
+            s(0.0000001)
